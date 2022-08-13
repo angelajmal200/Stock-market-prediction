83023bf0abab39d1d6a1477e45f6d3c69582cd49
AJMAL JOHNSON <angelajmal200@gmail.com>
2022-08-14 01:26:46 +0530

    registration completed

--- project workspace/bigbull/base/views.py
+++ project workspace/bigbull/base/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    name="Big Bull"
+    content={'name':name}
+    return render(request,"base\home1.html",content)
+
+def register(request):
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        password=request.POST['password']
+        user=User.objects.create_user(username=name,password=password,email=email)
+        user.save()
+        print("user created")
+        return render(request,"base\dashboard.html")
+    else:
+        return render(request,"base\index.html")
--- project workspace/bigbull/bigbull/settings.py
+++ project workspace/bigbull/bigbull/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'bigbull.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR /'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'bigbull.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'bigbull',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -116,6 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+MEDIA_URL = 'images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
