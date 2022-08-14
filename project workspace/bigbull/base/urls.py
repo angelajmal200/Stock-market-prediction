85e093499d92f5011e1e05da3e2ead2f258c2296
AJMAL JOHNSON <angelajmal200@gmail.com>
2022-08-14 22:27:53 +0530

    registration login check complete

--- project workspace/bigbull/base/urls.py
+++ project workspace/bigbull/base/urls.py
@@ -4,6 +4,10 @@ from .import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('register',views.register,name="register"),
-
+    path('login',views.login,name="login"),
+    path('dashboard',views.dashboard,name="dashboard"),
+    path('logout',views.logout,name="logout"),
+    path('header',views.header,name="header"),
+    path('snews',views.snews,name="snews"),
     
 ]
--- project workspace/bigbull/base/views.py
+++ project workspace/bigbull/base/views.py
@@ -1,22 +1,69 @@
+from contextlib import redirect_stderr
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+
+from gnewsclient import gnewsclient
+
 # Create your views here.
 def home(request):
     name="Big Bull"
     content={'name':name}
     return render(request,"base\home1.html",content)
 
+def dashboard(request):
+    return render(request,"base\dashboard.html")
+
 def register(request):
     if request.method=="POST":
         name=request.POST['name']
         email=request.POST['email']
         password=request.POST['password']
-        user=User.objects.create_user(username=name,password=password,email=email)
-        user.save()
-        print("user created")
-        return render(request,"base\dashboard.html")
+        if User.objects.filter(username=name).exists():
+            messages.info(request,"username taken")
+            return render(request,"base\index.html")
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,"email alredy exist")
+            return render(request,"base\index.html")
+        else:
+            user=User.objects.create_user(username=name,password=password,email=email)
+            user.save()
+            print("user created")
+            messages.info(request,"Thank You For Registration ❤️")
+            return render(request,"base\index.html")
     else:
         return render(request,"base\index.html")
+
+def login(request):
+    if request.method=="POST":
+        name=request.POST['name']
+        password=request.POST['password']
+        print(name)
+        print(password)
+        user=auth.authenticate(username=name,password=password)
+        if user is not None:
+            auth.login(request,user)
+            print("login sucess")
+            return redirect('dashboard')
+        else:
+            messages.info(request,"credentials invalid")
+            print("credentials invalid")
+            return render(request,"base\index.html")
+    else:
+        return render(request,"base\index.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'base\home1.html')
+
+def header(request):
+    return render(request,'header.html')
+
+def snews(request):
+    client = client = gnewsclient.NewsClient(language='english', location='india', topic='Buisness', use_opengraph=True, max_results=25)
+    news_list = client.get_news()
+    content={'news_list':news_list}
+    return render(request,"base\snews.html",content)
